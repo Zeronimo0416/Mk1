fcd4a20c9fbeb9a87cbfe4e3a8d6ef3abcf983cc
Zeronimo0416 <58081842+Zeronimo0416@users.noreply.github.com>
2019-12-03 20:05:04 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,224 @@
+<!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv=“refresh” content=“1; url=/index.html”>
-</head>
-</html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styles.css" />
+    .smw__home-contatiner {
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-image: url("https://buffer.com/resources/wp-content/uploads/2016/02/photo-1432821596592-e2c18b78144f.jpeg");
+  background-position: center;
+  height: 730px;
+}
+h1 {
+  font-size: 125px;
+  text-align: center;
+  opacity: 0.9;
+}
+h2 {
+  font-size: 25px;
+  text-align: center;
+}
 
-{ }
+.smw__mainbar {
+  position: fixed;
+  z-index: 1;
+  width: 98%;
+  height: 100px;
+  background-color: #f7f4ef;
+  display: flex;
+  border-radius: 18px;
+  margin-left: 1%;
+  margin-top: 1%;
+  box-shadow: 5px 15px 25px rgba(0, 0, 0, 0.5);
+  opacity: 0.9;
+}
+
+a {
+  text-decoration: none;
+  color: #454541;
+  font-size: 20px;
+  font-weight: 500;
+}
+a:hover {
+  color: black;
+  font-size: 20.5px;
+}
+.smw__homeimage {
+  width: 70%;
+}
+
+.smw__maincontents {
+  position: relative;
+  margin-left: 3%;
+  margin-top: 1%;
+}
+.smw__home__column {
+  width: 18%;
+  margin-left: 3%;
+  margin-top: 0.5%;
+}
+.smw__list__column {
+  width: 75%;
+  margin-left: 3%;
+}
+.smw__lists {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 42px;
+  font-size: 18px;
+}
+.smw__title {
+  padding: 100px;
+  align-items: center;
+}
+
+.home__home {
+  width: 530px;
+  margin-left: 395px;
+  margin-top: 150px;
+}
+
+  </head>
+  <body>
+    <div class="smw__home-contatiner">
+      <div class="smw__mainbar">
+        <div class="smw__home__column">
+          <img class="smw__homeimage" src="comeback2.png" />
+        </div>
+        <div class="smw__list__column">
+          <div class="smw__lists">
+            <div class="smw__inside1">
+              <a href="#">Home</a>
+            </div>
+            <div class="smw__inside2">
+              <a href="#">
+                What we offer
+              </a>
+            </div>
+            <div class="smw__inside3">
+              <a
+                href="#container"
+                >Contents
+              </a>
+            </div>
+            <div class="smw__inside4">
+              <a href="https://www.youtube.com/user/KMU1010">Youtube </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="smw__title">
+        <img class="home__home" src="comeback2.png" />
+      </div>
+    </div>
+    <div class="ji__whatweoffer-container">
+      <div class="ji__header">
+        <div class="ji__header-text">
+          <br />
+          WHAT WE OFFER
+        </div>
+      </div>
+
+      <div class="ji__maincolumn">
+        <div class="ji__content__column">
+          <div class="ji__circle__column">
+            <img
+              class="ji__offer__icon"
+              src="iteam__icon.png"
+              width="100"
+              height="100"
+            />
+          </div>
+          <div class="ji__circle__column">
+            <img class="ji__offer__icon" src="mis__icon.png" />
+          </div>
+          <div class="ji__circle__column">
+            <img class="ji__offer__icon" src="it__icon.png" />
+          </div>
+          <div class="ji__circle__column">
+            <img class="ji__offer__icon" src="book__icon.png" />
+          </div>
+        </div>
+      </div>
+        <div class="ji__footer">
+          <div class="ji__text__column">
+            <div class="ji__text__box-1">
+              Members
+            </div>
+            <div class="ji__text__box-2">
+              About MIS
+            </div>
+            <div class="ji__text__box-3">
+              IT related MIS
+            </div>
+            <div class="ji__text__box-4">
+              Book Reviews
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contents__container">
+        <div class="header">
+          <div class="header__text">
+            <div class="textbox">
+              CONTENTS
+            </div>
+          </div>
+        </div>
+        <div id="container">
+          <div class="container__pics-1">
+            <div class="team">
+              <a href="Introducing.html">
+              <img class="team__image" src="introduce.jpg" />
+              <div class="pic__text">Team members</div>
+            </a>
+            </div>
+            <div class="about"><a href="About MIS.html">
+
+              <img class="about__image" src="mismis.jpeg" />
+              <div class="pic__text">About MIS</div>
+            </a>
+            </div>
+          </div>
+          <div class="container__pics-2">
+            <div class="related"><a href="IT related MIS.html">
+
+              <img class="related__image" src="bigdata.jpg" />
+              <div class="pic__text">IT related MIS</div>
+            </a>
+            </div>
+            <div class="book"><a href="Book reviews.html">
+
+              <img class="book__image" src="reading books.jpg" />
+              <div class="pic__text">book Reviews</div>
+            </a>
+            </div>
+          </div>
+        </div>
+        <div class="ji__container">
+            <div class="ji__header">
+                <div class="ji__main">
+                    <img class="home__white-logo" src = "Logo.png"  width = "300" height = "100">
+                    <div class="container__text">
+                      <div class="ji__address__korean">
+                         서울특별시 성북구 정릉로 77 국민대학교 경영관 413호 [02707]
+                      </div>
+                      <div class="ji__address__english">
+                        MANAGEMENT CLASSROOM 413. KOOKMIN UNIVERSITY, 77
+                          JEONGNEUNG-R0, SEONGBUK-GU, SEOUL 02707, KOREA
+                      </div>
+                      <div class="ji__copyright">
+                        COPYRIGHT ⓒ 'We Want to Go Home!'. ALL RIGHT RESERVED
+                      </div>
+
+                    </div>
+                    
+                    </div>
+                </div>
+            </div>
+      </div>
+  </body>
+</html>
